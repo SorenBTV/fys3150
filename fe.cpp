f30c4c3130b3f1dc886241dd0836c3ef89341a39
SorenBTV <sorenbt96@gmail.com>
2022-08-24 13:17:00 +0200

    git commit

--- fe.cpp
+++ fe.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <cmath>
 
+double f(double x); //Declaration
 
-double f( double x);
-int main()
-{
+int main() {
     double x = 2;
-    double res = f(x);
+    double res = f(x); //Should return 2*2 = 4.
     return 0;
+}
+
+double f(double x){
+    //Provide definition of function here:
+    return x*x;
 }
