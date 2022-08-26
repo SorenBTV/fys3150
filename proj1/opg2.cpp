26139706758c12ecc58f8b396bc6b553db06209c
SorenBTV <sorenbt96@gmail.com>
2022-08-26 14:28:50 +0200

    git commit

--- proj1/opg2.cpp
+++ proj1/opg2.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 #include <armadillo>
+#include <string>
+#include <fstream>
+#include <iomanip>
 
+arma::vec u(arma::vec);
 
-vec x = linspace(int a, int b, int n); // declaration
 
 int main(){
-    int a = 0
-    int b = 1
-    int n = 51
-    vec x = linspace(a,b,n)
-   std::cout<<vec x <<"\n";
-   return 0;
-   }
+    
+    // std::string filename = "x_u.txt";
+    
+    // std::ofstream ofile;
+    // ofile.open(filename);
+    
+    arma::vec x;
+    
+    x = arma::linspace(0, 1, 100);
+    arma::vec res = u(x);
+    std::cout << res <<"\n";
+    return 0;
+}
+
+
+arma::vec u(arma::vec x_in){
+    return 1-(1-exp(-10)*x_in)-exp(-10*x_in);
+}
