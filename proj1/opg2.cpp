2655e81a78646af3649aaa88c3e912921afc5c38
SorenBTV <sorenbt96@gmail.com>
2022-08-29 09:41:02 +0200

    git commit

--- proj1/opg2.cpp
+++ proj1/opg2.cpp
@@ -9,16 +9,26 @@ arma::vec u(arma::vec);
 
 int main(){
     
-    // std::string filename = "x_u.txt";
-    
-    // std::ofstream ofile;
-    // ofile.open(filename);
-    
     arma::vec x;
     
     x = arma::linspace(0, 1, 100);
     arma::vec res = u(x);
     std::cout << res <<"\n";
+
+    std::string filename = "x_u.txt";
+    
+    std::ofstream ofile;
+    ofile.open(filename);
+    int width = 12;
+    int prec = 4;
+
+    for (int i = 0; i<=100; i++){
+        ofile << std::setw(width) << std::setprecision(prec) << std::scientific << x(i)
+          << std::setw(width) << std::setprecision(prec) << std::scientific << res(i)
+          << std::endl;
+    }
+    ofile.close();
+
     return 0;
 }
 
