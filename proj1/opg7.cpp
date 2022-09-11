0f26c9e462dea14339ee71b52c43962cc591cf67
SorenBTV <sorenbt96@gmail.com>
2022-09-11 17:43:21 +0200

    git commit

--- proj1/opg7.cpp
+++ proj1/opg7.cpp
@@ -5,7 +5,7 @@
 #include <iomanip>
 
 arma::vec f(arma::vec x);
-arma::vec x;
+arma::vec x, v, x1;
 arma::vec solve(arma::vec a, arma::vec b, arma::vec c, arma::vec g, int n);
 
 int main(){
@@ -20,9 +20,29 @@ double h = 1/n1;
 
 
 x = arma::linspace(0.2, 0.8, n);
+arma::vec x1 = arma::linspace(0, 1, n+2);
 arma::vec g = f(x)*h*h;
 arma::vec res = solve(a, b, c, g, n);
-std::cout << res << "\n";
+v = arma::linspace(0,0,n+2);
+for (int i = 0; i < n; i++){
+    v[i+1] = res[i];
+}
+std::cout << v << "\n";
+
+// Creation of .txt file with the outputs from our results.
+std::string filename = "x_v.txt";
+
+std::ofstream ofile;
+ofile.open(filename);
+int width = 12;
+int prec = 4;
+
+for (int i = 0; i<=100; i++){
+    ofile << std::setw(width) << std::setprecision(prec) << std::scientific << x1(i)
+          << std::setw(width) << std::setprecision(prec) << std::scientific << v(i)
+          << std::endl;
+    }
+    ofile.close();
 
 return 0;
 }
