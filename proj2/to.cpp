debcefd8a5ea44238b36100edbf6825142096824
SorenBTV <sorenbt96@gmail.com>
2022-09-21 20:01:30 +0200

    git commit

--- proj2/to.cpp
+++ proj2/to.cpp
@@ -11,10 +11,11 @@ arma::vec x, xh, v;
 arma::mat eigvec;
 arma::mat matrix(double a, double d, int N, arma::mat);
 arma::vec ana_eigval(int N, double a, double d);
+arma::mat ana_eigvec(int N);
 
 int main(){
 // Defining variables
-int n = 6;
+int n = 7;
 int N = n-1;
 int L = 1;
 double h = 1./(n);
@@ -25,24 +26,24 @@ arma::vec x = arma::linspace(0, L, n);
 arma::vec xh = arma::vec(n).fill(0.);
 arma::vec v = arma::vec(n).fill(0.);
 
-for (int i=0 ; i<=n; i++){
-xh[i] = xh[0] + i * h;
-}
+for (int i=0 ; i<=n; i++)
+{    xh[i] = xh[0] + i * h;}
 
 
 //Defining matrix and calling function
 arma::mat A = arma::mat(N, N);
-arma::mat mat = matrix(a, d, N, A);
-//std::cout << mat << "\n";
+arma::mat B = matrix(a, d, N, A);
+std::cout << B << "\n";
 
 arma::vec eigval;
 arma::mat eigvec;
-arma::eig_sym(eigval, eigvec, mat);
+arma::eig_sym(eigval, eigvec, B);
 
-std::cout << eigval << "\n";
+std::cout << eigvec << "\n";
 
-arma::vec ana_eigv = ana_eigval(n, a, d);
-std::cout << ana_eigv << "\n";
+arma::vec ana_eigv = ana_eigval(N, a, d);
+arma::mat ana_v = ana_eigvec(N);
+std::cout << ana_v << "\n";
 
 return 0;
 }
@@ -67,10 +68,23 @@ return A;
 }
 
 arma::vec ana_eigval(int N, double a, double d){
-    arma::vec eig = arma::vec(N).fill(0);
+    arma::vec eig = arma::vec(N).fill(0.);
 
         for (int i=1 ; i<=N ; i++){
-            eig[i] = d + 2*a*std::cos(i*pi/(N+1));
+            eig[i-1] = d + 2*a*std::cos(i*pi/(N+1));
             }
     return eig;
+}
+
+arma::mat ana_eigvec(int N){
+    arma::mat v = arma::mat(N,N).fill(0);
+
+    for(int i=1; i<=N; i++){
+        arma::vec v_i = arma::vec(N).fill(0);
+        for(int j=1; j<=N; j++){
+            v_i[j-1] = std::sin((j*i*pi)/(N+1));
+        }
+    v.col(i-1) = arma::normalise(v_i);
+    }
+return v;
 }
