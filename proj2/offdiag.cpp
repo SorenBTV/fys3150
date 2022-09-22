acf761fea9df9e61a0bc8089e615fd328bd27c31
SorenBTV <sorenbt96@gmail.com>
2022-09-22 13:51:17 +0200

    git commit

--- proj2/offdiag.cpp
+++ proj2/offdiag.cpp
@@ -20,22 +20,24 @@ return 0;
 
 
 //Functions
-
 double max_offdiag_symmetric(const arma::mat& A, int& k, int &l){
     int N = A.n_cols;
     double max_value = 0;
 
     for (int i=0; i<N; i++){
-        for (int j=i+1; j<N; j++){
 
-            if(i==j){max_value = max_value;}
+        for (int j = i+1; j<N; j++){
 
-            else{
-                if(std::abs(A(i,j)) >= max_value){
-                    max_value = std::abs(A(i,j));
-                    k=i;
-                    l=j;}}
-            }}
+            // Changes max_value to the highest absolute value in the off-diagonal and 
+            // sets k and l to the respective row and column of the max value
+            if(std::abs(A(i,j)) >= max_value){
+                max_value = std::abs(A(i,j));
+                k = i;
+                l = j;
+                }
+            else{max_value = max_value;}
+        }
+    }
     return max_value;
     }
 
@@ -49,7 +51,6 @@ void max_offdiag_symmetric_test(){
     double max_value;
 
     max_value = max_offdiag_symmetric(A, k, l);
-    std::cout << max_value << "\n";
     assert(max_value - 0.7 <= 1e-7);
     assert(k == 1);
     assert(l == 2);
