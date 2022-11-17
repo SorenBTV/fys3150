6095472cf70706051308ab914711a3f739047c87
SorenBTV <sorenbt96@gmail.com>
2022-11-17 17:44:00 +0100

    git commit

--- proj4/main.cpp
+++ proj4/main.cpp
@@ -1,15 +1,7 @@
-#include <iostream>
-#include <armadillo>
-#include <cmath>
-#include <fstream>
-#include <iomanip>
-#include <chrono>
-#include <random>
 // including the header and function files containing our classes
 #include "omp.h"
 #include "Lattice.hpp"
 
-using namespace std;
 
 /*
 Make:
@@ -19,14 +11,62 @@ g++ main.cpp src/Lattice.cpp -I include/ -o main.exe -fopenmp -larmadillo -Wall 
 int main()
 {
     
-    int L = 5;
-    double T = 1;
-    int seed = -1;
-    Lattice mysystem = Lattice(L, T);
-    // mysystem.fill_lattice(seed);
-    //cout << mysystem.spin_matrix << endl;
-    //cout << Lattice::spin_matrix << endl;
+    int L = 2.0;
+    double T = 1.0;
+    int MC_cycles = 10000;
+    bool ordered = true;
+    int threads = 4;
+
+    int seed = 123;
+    int N = L * L;
+
+    //double T_final = 1;
+    //double dt = (T_final - T)/N;
     
+    /*
+    // Problem 4
+    Lattice mysystem;
+    mysystem.Initializer(L, 2.4, MC_cycles, false);
+    mysystem.MCMC();
+    //cout << mysystem.spin_matrix_<< endl;
+    //mysystem.write_file_problem4();
+    
+
+
+
+    //Problem 5
+    Lattice mysystem2;
+    mysystem2.Initializer(20, 1.0, 100000, false);
+    mysystem2.MCMC_burn_in_time("problem_5_T1.0_cycles10000_not_ordered.txt");
+
+    Lattice mysystem3;
+    mysystem2.Initializer(20, 1.0, 100000, true);
+    mysystem2.MCMC_burn_in_time("problem_5_T1.0_cycles10000_ordered.txt");
+
+    Lattice mysystem4;
+    mysystem2.Initializer(20, 2.4, 100000, false);
+    mysystem2.MCMC_burn_in_time("problem_5_T2.4_cycles10000_not_ordered.txt");
+
+    Lattice mysystem5;
+    mysystem2.Initializer(20, 2.4, 100000, true);
+    mysystem2.MCMC_burn_in_time("problem_5_T2.4_cycles10000_ordered.txt");
+    */
+
+
+    //Problem 6
+    
+
+
+
+    //Problem 7,8
+
+
+    //#pragma omp parallel{omp_set_num_threads(threads);}
+    
+
+
+
+
 
     return 0;
 }
